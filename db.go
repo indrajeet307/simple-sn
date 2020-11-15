4de3c69dac197c471581d4a58955112518a67650
Indrajeet Khandekar <indrajeet.khandekar@taranawireless.com>
2020-11-15 16:01:04 +0530

    Add a gorm interface

--- db.go
+++ db.go
@@ -1,26 +1,35 @@
 package main
 
 import (
-	"errors"
-	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/gorm"
+	gl "gorm.io/gorm/logger"
+	"gorm.io/driver/sqlite"
 )
 
 type Database struct {
 	users     map[string]NewUserRequest
 	comments  map[int64]NewCommentRequest
 	reactions map[int64]ReactionRequest
+	engine *gorm.DB
 }
 
 func (db *Database) AddUser(nu *NewUserRequest) (err error) {
-	numUsers := len(db.users)
-	nu.ID = int64(numUsers)
-	_, found := db.users[nu.Email]
-	if found {
-		return errors.New(fmt.Sprintf("Email %s already registered", nu.Email))
+	user := User{
+	    Name: nu.Name,
+	    Email: nu.Email,
+	    Password: nu.Password,
 	}
-	db.users[nu.Email] = *nu
+	result := db.engine.Create(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+	nu.ID = user.Id
 	return nil
 }
+
 func (db *Database) AddComment(nc *NewCommentRequest) {
 	numComments := len(db.comments)
 	nc.ID = int64(numComments)
@@ -58,18 +67,44 @@ func (db *Database) GetCommentReactions(cid int64) (lr ListReactions) {
 
 var db *Database = nil
 
+type User struct {
+	Id int64
+	Name string `gorm:"notnull"`
+	Email string `gorm:"unique; notnull"`
+	Password string `gorm:"notnull"`
+	Active bool `gorm:"notnull; default:false"`
+	Created int64 `gorm:"autoCreateTime"`
+}
+
 func GetDB() *Database {
+	engine, err := gorm.Open(sqlite.Open(DBFILE), &gorm.Config{
+		Logger: gl.Default.LogMode(gl.Warn),
+	})
+	if err != nil {
+		log.Fatalf("Could not open connection db exiting.")
+		return nil
+	}
+	err = engine.AutoMigrate(&User{})
+
+	if err != nil {
+		log.Fatalf("Could not create table %s", err.Error())
+		return nil
+	}
 	if db == nil {
 		db = &Database{
 			users:     map[string]NewUserRequest{},
 			comments:  map[int64]NewCommentRequest{},
 			reactions: map[int64]ReactionRequest{},
+			engine: engine,
 		}
 	}
 	return db
 }
 
+const DBFILE="./test-db"
+
 func NewDB() *Database {
+	os.Remove(DBFILE)
 	db = nil
 	return GetDB()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module huddle-app
 
 go 1.13
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	gorm.io/driver/sqlite v1.1.3
+	gorm.io/gorm v1.20.6
+)
